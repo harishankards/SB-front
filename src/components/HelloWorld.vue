c3ed2491762d5fd0f7a7cee345382b336131c31e
hs <hs@spritle.com>
2018-03-24 17:02:27 +0530

    FEAT compatibility check - Dashboard toolbar and navbar

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,12 @@
 <template>
   <v-container fluid>
-    <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-        <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
-      </v-layout>
-    </v-slide-y-transition>
+    <form>
+      <v-text-field v-model="name" label="Name" :counter="10" :error-messages="errors.collect('name')" v-validate="'required|max:10'" data-vv-name="name" required>
+      </v-text-field>
+      <v-btn @click="submit">submit</v-btn>
+      <v-btn @click="clear">clear</v-btn>
+
+    </form>
   </v-container>
 </template>
 
@@ -33,3 +27,57 @@ a {
   color: #42b983;
 }
 </style>
+
+<script>
+  export default {
+    $_veeValidate: {
+      validator: 'new'
+    },
+
+    data: () => ({
+      name: '',
+      email: '',
+      select: null,
+      items: [
+        'Item 1',
+        'Item 2',
+        'Item 3',
+        'Item 4'
+      ],
+      checkbox: null,
+      dictionary: {
+        attributes: {
+          email: 'E-mail Address'
+          // custom attributes
+        },
+        custom: {
+          name: {
+            required: () => 'Name can not be empty',
+            max: 'The name field may not be greater than 10 characters'
+            // custom messages
+          },
+          select: {
+             required: 'Select field is required'
+          }
+        }
+      }
+    }),
+
+    mounted () {
+      this.$validator.localize('en', this.dictionary)
+    },
+
+    methods: {
+      submit () {
+        this.$validator.validateAll()
+      },
+      clear () {
+        this.name = ''
+        this.email = ''
+        this.select = null
+        this.checkbox = null
+        this.$validator.reset()
+      }
+    }
+  }
+</script>
