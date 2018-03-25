3b23e549872ce11a0b3dbdc4f3f2f5f12158bc3b
hs <hs@spritle.com>
2018-03-25 20:14:04 +0530

    INIT - Initialise the project

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,36 @@
 <template>
-  <v-app>
-    <v-navigation-drawer
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      enable-resize-watcher
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      app
-      :clipped-left="clipped"
-    >
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-    </v-toolbar>
-    <v-content>
-      <router-view/>
-      
-    </v-content>
-    
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2018 Spritle Software</span>
-    </v-footer>
-  </v-app>
+  <div id="app" class="app">
+    <auth-layout v-if="isAuth"></auth-layout>
+    <layout v-else></layout>
+  </div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire'
-      }],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+  import Layout from 'components/layout/Layout'
+  import AuthLayout from 'components/layout/AuthLayout'
+  import VuesticPreLoader from 'vuestic-components/vuestic-preloader/VuesticPreLoader.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      VuesticPreLoader,
+      AuthLayout,
+      Layout
+    },
+    computed: {
+      isAuth () {
+        return this.$route.path.match('auth')
+      }
     }
-  },
-  name: 'App'
-}
+  }
 </script>
+
+<style lang="scss">
+  @import "sass/main";
+  body {
+    height: 100%;
+    #app {
+      height: 100%;
+    }
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,27 +1,44 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VeeValidate from 'vee-validate'
 import App from './App'
+import store from './store'
 import router from './router'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+import { sync } from 'vuex-router-sync'
+import VuesticPlugin from 'vuestic-theme/vuestic-plugin'
+import './i18n'
 
-Vue.use(Vuetify, { theme: {
-  primary: '#ee44aa',
-  secondary: '#424242',
-  accent: '#82B1FF',
-  error: '#FF5252',
-  info: '#2196F3',
-  success: '#4CAF50',
-  warning: '#FFC107'
-}})
+Vue.use(VuesticPlugin)
 
-Vue.config.productionTip = false
+// NOTE: workaround for VeeValidate + vuetable-2
+Vue.use(VeeValidate, {fieldsBagName: 'formFields'})
+
+sync(store, router)
+
+let mediaHandler = () => {
+  if (window.matchMedia(store.getters.config.windowMatchSizeLg).matches) {
+    store.dispatch('toggleSidebar', true)
+  } else {
+    store.dispatch('toggleSidebar', false)
+  }
+}
+
+router.beforeEach((to, from, next) => {
+  store.commit('setLoading', true)
+  next()
+})
+
+router.afterEach((to, from) => {
+  mediaHandler()
+  store.commit('setLoading', false)
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  store,
+  template: '<App/>',
+  components: { App }
 })
