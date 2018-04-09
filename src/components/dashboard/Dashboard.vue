07c5380a28dcb3e8565074df4bc4df7a7c04a7e4
hs <hs@spritle.com>
2018-04-09 18:20:53 +0530

    HAND - REmove features and profile setup widget from dashboard

--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -4,18 +4,12 @@
     <dashboard-info-widgets></dashboard-info-widgets>
 
     <vuestic-widget class="no-padding no-v-padding">
-      <vuestic-tabs :names="[$t('dashboard.dataVisualization'), $t('dashboard.usersAndMembers'), $t('dashboard.setupProfile'), $t('dashboard.features')]" ref="tabs">
+      <vuestic-tabs :names="[$t('dashboard.dataVisualization'), $t('dashboard.usersAndMembers')]" ref="tabs">
         <div :slot="$t('dashboard.dataVisualization')">
           <data-visualisation-tab></data-visualisation-tab>
         </div>
         <div :slot="$t('dashboard.usersAndMembers')">
           <users-members-tab></users-members-tab>
-        </div>
-        <div :slot="$t('dashboard.setupProfile')">
-          <setup-profile-tab></setup-profile-tab>
-        </div>
-        <div :slot="$t('dashboard.features')">
-          <features-tab></features-tab>
         </div>
       </vuestic-tabs>
     </vuestic-widget>
@@ -28,8 +22,6 @@
 <script>
   import DashboardInfoWidgets from './DashboardInfoWidgets'
   import UsersMembersTab from './users-and-members-tab/UsersMembersTab.vue'
-  import SetupProfileTab from './setup-profile-tab/SetupProfileTab.vue'
-  import FeaturesTab from './features-tab/FeaturesTab.vue'
   import DataVisualisationTab from './data-visualisation-tab/DataVisualisation.vue'
   import DashboardBottomWidgets from './DashboardBottomWidgets.vue'
 
@@ -39,8 +31,6 @@
       DataVisualisationTab,
       DashboardInfoWidgets,
       UsersMembersTab,
-      SetupProfileTab,
-      FeaturesTab,
       DashboardBottomWidgets
     },
 
