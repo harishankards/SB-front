d873e57bf41c8faaeb0cc062cf80e13783a29178
hs <hs@spritle.com>
2018-04-09 18:26:32 +0530

    HAND - REmove data visualisation and users members and bottom widgets

--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -4,34 +4,20 @@
     <dashboard-info-widgets></dashboard-info-widgets>
 
     <vuestic-widget class="no-padding no-v-padding">
-      <vuestic-tabs :names="[$t('dashboard.dataVisualization'), $t('dashboard.usersAndMembers')]" ref="tabs">
-        <div :slot="$t('dashboard.dataVisualization')">
-          <data-visualisation-tab></data-visualisation-tab>
-        </div>
-        <div :slot="$t('dashboard.usersAndMembers')">
-          <users-members-tab></users-members-tab>
-        </div>
-      </vuestic-tabs>
+      
     </vuestic-widget>
 
-    <dashboard-bottom-widgets></dashboard-bottom-widgets>
 
   </div>
 </template>
 
 <script>
   import DashboardInfoWidgets from './DashboardInfoWidgets'
-  import UsersMembersTab from './users-and-members-tab/UsersMembersTab.vue'
-  import DataVisualisationTab from './data-visualisation-tab/DataVisualisation.vue'
-  import DashboardBottomWidgets from './DashboardBottomWidgets.vue'
 
   export default {
     name: 'dashboard',
     components: {
-      DataVisualisationTab,
-      DashboardInfoWidgets,
-      UsersMembersTab,
-      DashboardBottomWidgets
+      DashboardInfoWidgets
     },
 
     methods: {
