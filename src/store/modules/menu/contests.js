29ae1c661211183486e4d19b64df670e9677d2fd
hs <hs@spritle.com>
2018-04-14 18:01:13 +0530

    FIX - remove child components for contests and bring into one

--- src/store/modules/menu/contests.js
+++ src/store/modules/menu/contests.js
@@ -2,28 +2,11 @@ import lazyLoading from './lazyLoading'
 
 export default {
   name: 'Contests',
+  path: '/contests',
+  component: lazyLoading('contests/Contests'),
   meta: {
     expanded: false,
     title: 'menu.contests.parent',
     iconClass: 'fa fa-flag'
-  },
-
-  children: [
-    {
-      name: 'menu.contests.upcoming',
-      path: '/contests/upcoming',
-      component: lazyLoading('contests/Upcoming'),
-      meta: {
-        title: 'menu.contests.upcoming'
-      }
-    },
-    {
-      name: 'menu.contests.history',
-      path: '/contests/history',
-      component: lazyLoading('contests/History'),
-      meta: {
-        title: 'menu.contests.history'
-      }
-    }
-  ]
+  }
 }
