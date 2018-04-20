80e2bdbec633b0902b50a87c084f2ea561a08303
hs <hs@spritle.com>
2018-04-20 05:44:09 +0530

    FEAT - Add company newsfeed and templates

--- src/components/company/newsfeed/Newsfeed.vue
+++ src/components/company/newsfeed/Newsfeed.vue
@@ -1,17 +1,134 @@
 <template>
-  <div class="align-center">
-    <h3>come back later dude ;)</h3>
+  <div class="dashboard">
+
+    <newsfeed-info-widgets></newsfeed-info-widgets>
+
+    <div class="row">
+      <div class="col-md-8">
+        <vuestic-widget class="" v-for="post in posts" :key="post.id">
+          <div >
+            <p class="feed-card-info"> <strong> {{post.by}}</strong> {{post.action}} this</p>
+            <hr>
+          </div>
+          <div>
+            <div id="feed-card-image">{{giveInitial(post)}}</div>
+            <div id="feed-card-user-name-div">
+              <span class="feed-card-username"><strong><a href="#">{{post.name}}</a> </strong></span><br>
+              <span class="feed-card-time">{{post.calender}}</span>
+            </div>
+          </div>
+          <div id="post-content-div">
+            <p id="post-description">{{post.post_desc}}</p>
+          </div>
+          <hr>
+          <div id="user-interaction-div">
+            <div>
+              <button class=""> <i class="fa fa-thumbs-up"></i> Upvote</button>
+            </div>
+            <div id="comments-div">
+              <vue-disqus shortname="student-burger"></vue-disqus>
+            </div>
+          </div>
+          
+        </vuestic-widget>
+      </div>
+      <div class="col-md-4">
+        <vuestic-widget class="live-feed" headerText="Live feeds">
+          <vuestic-feed class="newsfeed-page" :initialPosts="posts"></vuestic-feed>
+        </vuestic-widget>
+      </div>
+    </div>
+
+
   </div>
 </template>
 
 <script>
-  export default{
-    name: 'companyNewsfeed'
+  import VueDisqus from 'vue-disqus/VueDisqus.vue'
+  import NewsfeedInfoWidgets from './NewsfeedInfoWidgets'
+
+  export default {
+    name: 'dashboard',
+    components: {
+      NewsfeedInfoWidgets,
+      VueDisqus
+    },
+    data () {
+      return {
+        posts: [
+          {
+            id: 0,
+            photoURL: 'https://goo.gl/KnVxVY',
+            name: 'Harishankar',
+            text: 'is going for a contest',
+            action: 'upvoted',
+            by: 'Balaji',
+            calender: '32m',
+            post_desc: 'Happy to upload my final year project'
+          },
+          {
+            id: 1,
+            photoURL: 'https://goo.gl/1nKusR',
+            name: 'Balaji D Loganathan',
+            text: 'upvoted for a project',
+            action: 'commented',
+            by: 'Surendran',
+            calender: '3h',
+            post_desc: 'Uploaded my dream project'
+          },
+          {
+            id: 2,
+            photoURL: 'https://goo.gl/Ckaexc',
+            name: 'Surendran S',
+            text: 'upvoted for a project',
+            action: 'upvoted',
+            by: 'HS',
+            calender: '2d',
+            post_desc: 'Woohoo!!!! Finally uploaded the project ^_^'
+          }
+        ]
+      }
+    },
+
+    methods: {
+      giveInitial: function (post) {
+        let initial = post.name.charAt(0)
+        return initial
+      }
+    }
   }
 </script>
-  
-<style>
-  .align-center{
-    text-align: center;
+
+<style lang="scss" scoped>
+  @import "../../../sass/_variables.scss";
+  .newsfeed-page{
+    padding-left: 0rem !important;
   }
+  .feed-card-time{
+    margin-top: 3px;
+    color: #a29e9e;
+  }
+  #feed-card-image{
+    display: inline-block;
+    border: 1px solid #a29e9e;
+    border-radius: 50%;
+    background: #CA3C25;
+    font-size: 1.5rem;
+    height: 3rem;
+    width: 3rem;
+    padding-left: 0.95rem;
+    padding-top: 0.3rem;
+    color: #e8dfdf;
+    vertical-align: top;
+  }
+
+  #feed-card-user-name-div{
+    display: inline-block;
+    margin-left: 0.4rem;
+  }
+
+  #post-content-div{
+    margin-top: 0.5rem;
+  }
+   
 </style>
---
+++ src/components/company/newsfeed/NewsfeedInfoWidgets.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="row dashboard-info-widgets">
+    <div class="col-md-8 col-xl-4"> 
+      <vuestic-widget class="info-widget">
+        <div class="uv-project-header">
+          <span class="uv-project-header-title"> Most upvoted project today </span>
+          <i class="fa fa-thumbs-up uv-project-header-icon"></i>
+        </div>
+        <div class="uv-project-content">
+          <div class="uv-project-title"><a href="#">Polymorphism in plants</a></div>
+          <div><small>This projects provides better analysis of polymorphism in plants and also the...</small></div>
+        </div>
+      </vuestic-widget>
+    </div>
+    <div class="col-md-8 col-xl-4">
+      <vuestic-widget class="info-widget">
+        <div class="searched-contest-header">
+          <span class="searched-contest-header-title"> Most searched contest today </span>
+          <i class="fa fa-bolt searched-contest-header-icon"></i>
+        </div>
+        <div class="searched-contest-content">
+          <div class="searched-contest-title"><a href="#">Google summer of code 2018</a></div>
+          
+        </div>
+      </vuestic-widget>
+    </div>
+    <div class="col-md-8 col-xl-4">
+      <vuestic-widget class="info-widget">
+        <div class="ad-header">
+          <span class="ad-header-title"> Hot topic around internet </span>
+          <i class="fa fa-fire ad-header-icon"></i>
+        </div>
+        <div class="ad-content">
+          <div class="ad-title"><a href="#">Freshworks launches a new product for students. Check it out now!</a></div>
+        </div>        
+
+      </vuestic-widget>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'dashboard-info-widgets'
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import "../../../sass/_variables.scss";
+
+  .stats-number, .stats-title {
+    line-height: 1;
+  }
+  .info-widget{
+    max-height: 80%;
+  }
+
+  .uv-project-header-title{
+    display: inline-block;
+    font-size: 1em; 
+  }
+  .uv-project-header-icon{
+    font-size: 2em;
+    float: right;
+  }
+
+  .uv-project-header{
+    padding-bottom: 2%;
+  }
+
+  .uv-project-title{
+    font-size: 1.5em;
+  }
+
+
+  .searched-contest-header-title{
+    display: inline-block;
+    font-size: 1em; 
+  }
+  .searched-contest-header-icon{
+    font-size: 2em;
+    float: right;
+  }
+
+  .searched-contest-header{
+    padding-bottom: 2%;
+  }
+
+  .searched-contest-title{
+    font-size: 1.5em;
+  }
+
+  .ad-header-title{
+    display: inline-block;
+    font-size: 1em; 
+  }
+  .ad-header-icon{
+    font-size: 2em;
+    float: right;
+  }
+
+  .ad-header{
+    padding-bottom: 2%;
+  }
+
+  .ad-title{
+    font-size: 1.1em;
+  }
+  .info-widget-inner {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    width: 100%;
+
+    &.has-chart {
+      justify-content: space-between;
+    }
+
+    .stats {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      height: 100%;
+    }
+  }
+
+  .stats-number {
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    font-size: 2.625rem;
+    margin-bottom: 0.5rem;
+
+    .stats-icon {
+      font-size: 1.5625rem;
+      position: absolute;
+      top: 0.625rem;
+      left: -1.25rem;
+
+      &.icon-wide {
+        left: -1.875rem;
+      }
+    }
+  }
+
+</style>
