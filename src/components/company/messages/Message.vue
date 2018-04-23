db594791b9f8ce407c530c9cf104bea21a35f00d
hs <hs@spritle.com>
2018-04-23 09:06:19 +0530

    FEAT - Add company messages widget

--- src/components/company/messages/Message.vue
+++ src/components/company/messages/Message.vue
@@ -1,9 +1,30 @@
 <template>
-  <h1>coming soon..</h1>
+  <vuestic-widget class="col-md-10">
+    <!-- <div class="" style="float:left; clear:none">
+      <h1>chat widgets</h1>
+    </div> -->
+    <div class="col-md-7 col-md-offset-3" style="">
+      <vuestic-chat v-model="chatMessages" height="10rem"></vuestic-chat>    
+    </div>    
+  </vuestic-widget>
 </template>
 
 <script>
   export default {
-    name: 'companymessages'
+    name: 'companymessages',
+    data () {
+      return {
+        chatMessages: [
+          {
+            text: 'Hello! Are you busy?', // Text of message
+            yours: false // Indicates if this message from you
+          },
+          {
+            text: 'Hi. For you - I\'m busy forever...',
+            yours: true
+          }
+        ]
+      }
+    }
   }
 </script>
