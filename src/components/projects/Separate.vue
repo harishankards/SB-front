7e9dd6a158b410062698597d383adf86abfc5f0f
hs <hs@spritle.com>
2018-04-27 05:17:53 +0530

    FEAT - Separate project page: Get the project content dynamically using route params

--- src/components/projects/Separate.vue
+++ src/components/projects/Separate.vue
@@ -1,20 +1,31 @@
 <template>
-	<h3>coming soon..</h3>
+	<vuestic-widget class="col-md-9" :headerText="this.projectData.title">
+    <p>{{this.projectData.abstract}}</p>
+    <p>{{this.projectData.description}}</p>
+    <hr>
+    <div>
+      <i class="fa fa-heart"></i> {{this.projectData.upvotes.length}} upvotes
+    </div>
+  </vuestic-widget>
 </template>
 
 <script>
   export default {
-    name: 'separate project',
+    name: 'separateProject',
     data () {
       return {
-        projectData: ''
+        projectData: '',
+        projectId: ''
       }
     },
     created () {
-      const projectId = this.$router.params
-      this.$http.get('projects/get/' + projectId)
-      .then(function (projectData) {
-        console.log('projectData', projectData)
+      var secondthis = this
+      const projectId = this.$route.params.id
+      this.projectId = projectId
+      this.$http.get('projects/get?id=' + projectId)
+      .then(function (projectDetails) {
+        console.log('projectData', projectDetails.data)
+        secondthis.projectData = projectDetails.data
       })
       .catch(function (projectDataErr) {
         console.log('projectdataerr', projectDataErr)
