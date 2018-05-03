27b264f0abb655490a513a311a8df76159114a34
hs <hs@spritle.com>
2018-05-03 09:28:03 +0530

    FEAT - Add 404 conditions to take company and student to their corresponding routes

--- src/components/404/NotFound.vue
+++ src/components/404/NotFound.vue
@@ -1,9 +1,32 @@
 <template>
-  <h1>Did you try to be smart?</h1>
+  <div class="container h-100">
+    <div class="row h-100 justify-content-center align-items-center landing-parent">
+      <div class="col-md-6">
+        <h3 class="col-md-8 offset-2">Did you try to be smart?</h3>
+      </div>
+      <div class="col-md-12">
+        <div class="button-div col-md-6 offset-5">
+          <a href="#" @click.prevent="takeHome">
+            <i class="fa fa-arrow-left"></i> Go back to home</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
-    name: 'notfound'
+    name: 'notfound',
+    methods: {
+      takeHome () {
+        if (this.$ls.get('student')) {
+          this.$router.push('/student/newsfeed')
+        } else if (this.$ls.get('company')) {
+          this.$router.push('/company/newsfeed')
+        } else {
+          this.$router.push('/')
+        }
+      }
+    }
   }
 </script>
