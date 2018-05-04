848fc01d21fe41f0ad635e88a7fab8657178f31c
hs <hs@spritle.com>
2018-05-04 08:49:55 +0530

    FEAT - Bring postsignup details in a wizard

--- src/components/profile/PostSignup.vue
+++ src/components/profile/PostSignup.vue
@@ -1,41 +1,70 @@
 <template>
   <div class="row">
-    <div class="col-md-9">
-      <vuestic-widget headerText="Update Profile">
-        <div class="form-group col-md-6">
+    <vuestic-widget class="col-md-10">
+      <vuestic-wizard 
+        :steps="vrSteps" 
+        wizard-layout="vertical" 
+        wizard-type="simple"
+        class="new">
+
+        <div slot="page1" class="form-wizard-tab-content">
+        <div class="form-group">
           <div class="input-group">
           <input id="simple-input" required/>
           <label class="control-label" for="simple-input">First Name</label><i class="bar"></i>
           </div>
         </div> 
-        <div class="form-group col-md-6">
+        <div class="form-group">
           <div class="input-group">
           <input id="simple-input" required/>
           <label class="control-label" for="simple-input">Last Name</label><i class="bar"></i>
           </div>
         </div> 
-        <div class="form-group col-md-6">
+        <div class="form-group">
           <div class="input-group">
           <input id="simple-input" required/>
           <label class="control-label" for="simple-input">Username</label><i class="bar"></i>
           </div>
         </div> 
-        <div class="form-group col-md-6 align-gender">
+        <div class="form-group align-gender">
           <div class="input-group">
             <div class="radio abc-radio abc-radio-primary">
               <input type="radio" name="male" id="male" value="male" checked>
               <label for="male">
                 <span class="abc-label-text">Male</span>
               </label>
-              <input type="radio" name="male" id="female" value="female" checked>
+              <input type="radio" name="female" id="female" value="female">
               <label for="female">
                 <span class="abc-label-text">Female</span>
+              </label>
+              <input type="radio" name="others" id="others" value="others">
+              <label for="others">
+                <span class="abc-label-text">Others</span>
               </label>
             </div>
           </div>
         </div>
-      </vuestic-widget>
-    </div>
+        </div>
+        <div slot="page2" class="form-wizard-tab-content">
+          <div class="form-group">
+            <div class="input-group">
+              <input id="simple-input" required/>
+              <label class="control-label" for="simple-input">College Name</label><i class="bar"></i>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="input-group">
+              <input id="simple-input" required/>
+              <label class="control-label" for="simple-input">University</label><i class="bar"></i>
+            </div>
+          </div> 
+        </div>
+        <div slot="wizardCompleted" class="form-wizard-tab-content">
+              <h4>Add slot="wizardCompleted" to your wizard's last step,
+              to show this step after wizard completed!</h4>
+        </div>
+      </vuestic-wizard>
+    </vuestic-widget>
   </div>
 </template>
 
@@ -44,7 +73,35 @@
     name: 'postSignup',
     data () {
       return {
-        isMale: ''
+          // how your steps should look like:
+        isMale: '',
+        vrSteps: [
+          {
+            label: 'Personal details',
+            slot: 'page1', // the same name as in attribute "slot" of wizard's step
+            onNext: () => {
+              // method is called when moving to the next step
+            },
+            isValid: () => {
+              // condition for moving to the next step
+              return true
+            }
+          },
+          {
+            label: 'Almameter',
+            slot: 'page2', // the same name as in attribute "slot" of wizard's step
+            onNext: () => {
+              // method is called when moving to the next step
+            },
+            isValid: () => {
+              // condition for moving to the next step
+              return true
+            },
+            onBack: () => {
+              // method is called when moving to the previous step
+            }
+          }
+        ]
       }
     }
   }
@@ -54,5 +111,11 @@
 <style scoped>
   .align-gender {
     padding-left: 1rem;
+  }
+  .new{
+    background-color: white;
+    padding-top: 15px;
+    padding-right: 15px;
+    padding-left: 15px;
   }
 </style>
