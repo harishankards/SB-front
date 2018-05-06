a053912dad72b8dc4ceb5233b3f5c5401dbd629c
hs <hs@spritle.com>
2018-05-06 19:34:51 +0530

    FEAT - Add separate contests view data dynamically

--- src/components/company/contest/Separate.vue
+++ src/components/company/contest/Separate.vue
@@ -1,13 +1,16 @@
 <template>
 	<vuestic-widget class="col-md-9" :headerText="this.contestData.title">
-    <p><strong> Abstract</strong><br>{{this.projectData.abstract}}</p>
-    <p><strong> Description</strong><br><span v-html="this.projectData.description"></span></p>
-    <p><strong> Author</strong><br> {{this.authorData.email}}</p>
-    <strong>Tags:</strong><span v-for="tag in projectData.tags" :key="tag.id" class="tagNames">{{tag.name}}</span>
+    <p><strong> About</strong><br>{{this.contestData.about}}</p>
+    <p><strong> Rules and Format</strong><br><span v-html="this.contestData.description"></span></p>
+    <p><strong> Hosted by</strong><br> {{this.hostData.email}}</p>
+    <p><strong>Starts on:</strong>  {{this.contestData.date.start | moment("dddd, MMMM Do YYYY, h:mm a")}}</p>
+    <p><strong>Ends on: </strong> {{this.contestData.date.end | moment("dddd, MMMM Do YYYY, h:mm a") }}</p>            
+    <strong>Tags:</strong><span v-for="tag in contestData.tags" :key="tag.id" class="tagNames">{{tag.name}}</span>
+    <p class="published">Published: <timeago :since="this.contestData.createdAt" :auto-update="60"></timeago></p>
     <hr>
     <div>
       <div class="comment-section">
-      <i class="fa fa-thumbs-up"></i> {{this.projectData.upvotes.length}} upvotes
+      <i class="fa fa-child"></i> {{this.contestData.registrations.length}} people registered
       </div>
       <div class="comment-section">
         <i class="fa fa-share"></i> Share
@@ -22,7 +25,7 @@
   import VueDisqus from 'vue-disqus/VueDisqus.vue'
 
   export default {
-    name: 'separateProject',
+    name: 'separateContest',
     components: {
       VueDisqus
     },
@@ -46,7 +49,7 @@
       .then(function (contestDetails) {
         console.log('contestData', contestDetails.data)
         secondthis.contestData = contestDetails.data
-        secondthis.$http.get('/company/get?id=' + contestDetails.data.host, {
+        secondthis.$http.get('/companies/get?id=' + contestDetails.data.host, {
           headers: {
             'Authorization': 'Bearer ' + authToken
           }
@@ -82,5 +85,9 @@
     background: #ff0081;
     color: white;
     border-radius: 5%;
+  }
+  .published {
+    color: gray;
+    margin-top: 1rem;
   }
 </style>
