6bdafe695c86d1b506c9b4ff55c5a9d687a1d6db
hs <hs@spritle.com>
2018-05-15 13:19:30 +0530

    FEAT - Add editor component for creating and editing company project description

---
+++ src/components/company/projects/Editor.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <vue-editor v-model="content" placeholder='Type your description here'></vue-editor>
+</template>
+
+ <script>
+  import { VueEditor } from 'vue2-editor'
+  import { eventBus } from '../../main.js'
+
+  export default {
+    name: 'editor',
+    components: {
+      VueEditor
+    },
+
+    data () {
+      return {
+        content: ''
+      }
+    },
+    updated () {
+      eventBus.$emit('editorContentproject', this.content)
+    },
+    created () {
+      if (this.$route.path.match('edit')) {
+        this.content = eventBus.projectToBeEdited.description
+      }
+    }
+  }
+ </script>
