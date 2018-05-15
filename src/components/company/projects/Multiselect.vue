5d4477a359ee394627ff7939249832a5439dbb79
hs <hs@spritle.com>
2018-05-15 13:21:13 +0530

    FEAT - Add Multiselecting component for creating and editing project tags

---
+++ src/components/company/projects/Multiselect.vue
@@ -0,0 +1,52 @@
+<template>
+   <multiselect v-model="value" tag-placeholder="Add this as new tag" placeholder="Search or add a tag" label="name" track-by="code" :options="options" :multiple="true" :taggable="true" @tag="addTag"></multiselect>
+</template>
+
+<script>
+
+import Multiselect from 'vue-multiselect'
+import { eventBus } from '../../main.js'
+
+export default {
+  components: {
+    Multiselect
+  },
+  data () {
+    return {
+      value: [
+        // { name: 'Computer Science', code: 'cse' }
+      ],
+      options: [
+        { name: 'Computer Science', code: 'cse' },
+        { name: 'Electronics', code: 'elec' },
+        { name: 'Mechanical', code: 'mech' },
+        { name: 'Biotechnology', code: 'Biot' }
+      ]
+    }
+  },
+  methods: {
+    addTag (newTag) {
+      const tag = {
+        name: newTag,
+        code: newTag.substring(0, 2) + Math.floor((Math.random() * 10000000))
+      }
+      console.log('tag', tag)
+      this.options.push(tag)
+      this.value.push(tag)
+    }
+  },
+  updated () {
+    eventBus.$emit('multiselectorproject', this.value)
+  },
+  created () {
+    if (this.$route.path.match('edit')) {
+      console.log('before matching')
+      console.log('yes matched ---------------------', eventBus.projectToBeEdited.tags)
+      this.value = eventBus.projectToBeEdited.tags
+    }
+  }
+}
+
+</script>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
