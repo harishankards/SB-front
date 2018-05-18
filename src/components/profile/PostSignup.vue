d5c12323035fff35a22e04b03f8900b893c98370
hs <hs@spritle.com>
2018-05-18 05:38:56 +0530

    FEAT - post signup validation template and tab wizards #WIP

--- src/components/profile/PostSignup.vue
+++ src/components/profile/PostSignup.vue
@@ -1,5 +1,32 @@
 <template>
+  <vuestic-widget>
+    <form-wizard @on-complete="onComplete"
+                  @on-loading="setLoading"
+                  @on-validate="handleValidation"
+                  @on-error="handleErrorMessage"
+                  shape="circle"
+                  color="gray"
+                  error-color="#e74c3c">
+      <tab-content title="Personal details"
+                  :before-change="validateAsync"
+                  icon="ti-user">
+              First tab
+      </tab-content>
+      <tab-content title="Additional Info"
+                  icon="ti-settings">
+              Second tab
+      </tab-content>
+      <tab-content title="Last step"
+                  icon="ti-check">
+              Third tab
+      </tab-content>
 
+      <div class="loader" v-if="loadingWizard"></div>
+      <div v-if="errorMsg">
+        <span class="error">{{errorMsg}}</span>
+      </div>
+    </form-wizard>
+  </vuestic-widget>
 </template>
 
 <script>
@@ -13,7 +40,38 @@
       TabContent
     },
     data () {
-      return {}
+      return {
+        loadingWizard: false,
+        errorMsg: null,
+        count: 0
+      }
+    },
+    methods: {
+      onComplete: function () {
+        alert('Yay. Done!')
+      },
+      setLoading: function (value) {
+        this.loadingWizard = value
+      },
+      handleValidation: function (isValid, tabIndex) {
+        console.log('Tab: ' + tabIndex + ' valid: ' + isValid)
+      },
+      handleErrorMessage: function (errorMsg) {
+        this.errorMsg = errorMsg
+      },
+      validateAsync: function () {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            if (this.count < 1) {
+              this.count ++
+              reject(new Error('This is a custom validation error message. Click next again to get rid of the validation'))
+            } else {
+              this.count = 0
+              resolve(true)
+            }
+          }, 1000)
+        })
+      }
     }
   }
 </script>
@@ -28,6 +86,12 @@
     padding-top: 15px;
     padding-right: 15px;
     padding-left: 15px;
+  }
+  span.error{
+    color:#e74c3c;
+    font-size:20px;
+    display:flex;
+    justify-content:center;
   }
 
 </style>
