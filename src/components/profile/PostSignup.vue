f5a2d28ea7e7a2ed17555a82a04dd080679a99a9
hs <hs@spritle.com>
2018-05-18 16:34:47 +0530

    FEAT - Add update student functionality on student post signup

--- src/components/profile/PostSignup.vue
+++ src/components/profile/PostSignup.vue
@@ -30,7 +30,11 @@
     },
     data () {
       return {
-        counter: 0
+        counter: 0,
+        studentId: '',
+        studentData: {
+          tags: []
+        }
       }
     },
     methods: {
@@ -45,7 +49,52 @@
         } else {
           return true
         }
+      },
+      updateStudent: function () {
+        const authToken = this.$ls.get('token')
+        this.studentData.tags = [
+          '5afce716bd80c12133c8d443',
+          '5afce69ebd80c12133c8d43a'
+        ]
+        console.log('this student data from update student', this.studentData)
+        this.$http.put('/students/update', this.studentData, {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + authToken
+          }
+        })
+        .then(function (studentDataUpdated) {
+          console.log('student data updated', studentDataUpdated)
+        })
+        .catch(function (studentDataUpdateErr) {
+          console.log('student data update err', studentDataUpdateErr)
+        })
       }
+    },
+    mounted () {
+      // const self = this
+      // setTimeout(() => {
+      //   self.updateStudent()
+      // }, 3000)
+    },
+    created () {
+      const self = this
+      const authToken = this.$ls.get('token')
+      const email = this.$ls.get('email')
+      this.$http.get('/students/get?email=' + email, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + authToken
+        }
+      })
+      .then(function (studentData) {
+        console.log('student data', studentData)
+        self.studentData = studentData.data[0]
+        console.log('student data main', self.studentData)
+      })
+      .catch(function (studentDataErr) {
+        console.log('student data err', studentDataErr)
+      })
     }
   }
 </script>
