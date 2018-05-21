890452595a31ffb423449921403f9bf26ab1d32d
hs <hs@spritle.com>
2018-05-21 18:32:35 +0530

    FEAT - Get the notifications page for students notification page and complete basic page

--- src/components/profile/Notifications.vue
+++ src/components/profile/Notifications.vue
@@ -1,9 +1,40 @@
 <template>
-  <h1>hello</h1>
+  <vuestic-widget class="col-md-8">
+    <div v-for="notification in this.notificationsData" :key="notification.id">
+      <a href="" @click.prevent="takeToNoti(notification)">{{notification.text}}</a>
+    </div>
+  </vuestic-widget>
 </template>
 
 <script>
   export default {
-    name: 'student-notifications'
+    name: 'student-notifications',
+    data () {
+      return {
+        notificationsData: ''
+      }
+    },
+    methods: {
+      takeToNoti: function (notification) {
+        if (notification.type === 'project') {
+          this.$router.push('/student/project/' + notification.link)
+        }
+      }
+    },
+    created () {
+      const self = this
+      this.$http.get('/students/get?email=' + this.$ls.get('email'), {
+        headers: {
+          'Authorization': 'Bearer ' + this.$ls.get('token')
+        }
+      })
+      .then(function (studentData) {
+        console.log('notifications data', studentData.data[0].notifications)
+        self.notificationsData = studentData.data[0].notifications
+      })
+      .catch(function (studentDataErr) {
+        console.log('student data err', studentDataErr)
+      })
+    }
   }
 </script>
