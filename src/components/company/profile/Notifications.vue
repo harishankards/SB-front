a891cb0ffc2e66ba2631dbee4d36836544d628f5
Manimala <manimala1144@gmail.com>
2018-06-06 12:02:08 +0530

    UI - Change design for notifications in profile for company

--- src/components/company/profile/Notifications.vue
+++ src/components/company/profile/Notifications.vue
@@ -1,7 +1,9 @@
 <template>
   <vuestic-widget class="col-md-8">
-    <div v-for="notification in this.notificationsData" :key="notification.id">
-      <a href="" @click.prevent="takeToNoti(notification)">{{notification.text}}</a>
+    <h5 class="headingStyle">Your Notifications</h5>
+    <div v-for="notification in this.notificationsData" :key="notification.id" class="notificationStyle" @click.prevent="takeToNoti(notification)">
+      <div class="iconStyle">{{firstletter(notification.text)}}</div>
+      <a href="" class="textStyle">{{notification.text}}</a>
     </div>
   </vuestic-widget>
 </template>
@@ -19,6 +21,10 @@
         if (notification.type === 'project') {
           this.$router.push('/company/project/' + notification.link)
         }
+      },
+      firstletter: function (text) {
+        console.log('inside first letter function', text)
+        return text.charAt(0).toUpperCase()
       }
     },
     created () {
@@ -38,3 +44,35 @@
     }
   }
 </script>
+<style scoped>
+.notificationStyle{
+  border-top: 1px solid lightgray;
+  padding-top: 1%;
+  padding-bottom: 2%;
+  cursor: pointer;
+}
+.iconStyle{
+  border-radius: 20px;
+  font-size: 100%;
+  margin-left: 5%;
+  display: inline-block;
+  padding-bottom: 2%;
+  width:40px;
+  height:40px;
+  font-size: 25px;
+  border:1px solid #000;
+  text-align: center;
+}
+.textStyle{
+   padding-bottom: 1%;
+   padding-left: 5%;
+   display: inline-block;
+}
+.notificationStyle:hover{
+  background-color: #D3E7DD;
+}
+.headingStyle{
+  margin-left: 4%;
+  padding-bottom: 1%;
+}
+</style>
