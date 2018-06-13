b9ec2841c2fe4df5c5f10a80351e94b7122945ac
Manimala <manimala1144@gmail.com>
2018-06-13 11:21:02 +0530

    FEAT - Validation for postsignup - Work in progress

--- src/components/profile/PostSignup.vue
+++ src/components/profile/PostSignup.vue
@@ -3,11 +3,13 @@
     <form-wizard @on-complete="onComplete" title="" subtitle="" color="#4ae387">
       <tab-content title="Personal details"
                   icon="fa fa-user"
-                  :before-change="personalCheck">
+                  :before-change="() => personalCheck('first')">
       <div class="form-inline">
           <div class="col-md-6">   
             <h6 class="labelStyle">First Name:</h6>           
-            <input type="text" class="inputboxStyle" v-model="personalData.fname" id="fname" required/>            
+            <input type="text" class="inputboxStyle" v-validate="'required|alpha'" :class="{'input': true, 'is-danger': errors.has('Firstname') }" name="Firstname" v-model="personalData.fname" id="fname"/><br>            
+            <i v-show="errors.has('first.Firstname')" class="fa fa-warning"></i>
+            <span v-show="errors.has('first.Firstname')" class="help is-danger">{{ errors.first('Firstname') }}</span>
           </div>
           <div class="col-md-6">
              <h6 class="labelStyle">Last Name:</h6>
@@ -17,12 +19,16 @@
           <div class="form-inline">
         <div class="col-md-6">
           <h6 class="labelStyle">Username:</h6>
-            <input type="text" class="inputboxStyle" v-model="personalData.uname" id="uname" required/>
+            <input type="text" class="inputboxStyle" v-validate="'required|alpha'" :class="{'input': true, 'is-danger': errors.has('first.Username') }" name="Username" v-model="personalData.uname" id="uname"/><br>
+            <i v-show="errors.has('first.Username')" class="fa fa-warning"></i>
+            <span v-show="errors.has('first.Username')" class="help is-danger">{{ errors.first('first.Username') }}</span>
         </div></div>
         <div class="form-inline">
          <div class="col-md-6">      
          <h6 class="labelStyle">Date Of Birth:</h6>
-         <input type="text" class="inputboxStyle" id="dob" v-model="personalData.dob" placeholder="DD-MM-YYYY" required/>
+         <input type="text" class="inputboxStyle" id="dob" v-validate="'required'" name="Date" :class="{'input': true, 'is-danger': errors.has('first.Date') }" v-model="personalData.dob" placeholder="DD-MM-YYYY" /><br>
+          <i v-show="errors.has('first.Date')" class="fa fa-warning"></i>
+          <span v-show="errors.has('first.Date')" class="help is-danger">{{ errors.first('first.Date') }}</span>
        </div></div>
        <div class="form-inline">
         <div class="col-md-6"> 
@@ -55,25 +61,33 @@
         <div class="form-inline">
         <div class="col-md-6">
             <h6 class="labelStyle">College Name:</h6>
-            <input type="text" class="inputboxStyle" v-model="academicData.collegeName" id="collegeName" required/>
+            <input type="text" class="inputboxStyle" v-validate="'required|alpha'" :class="{'input': true, 'is-danger': errors.has('CollegeName') }" name="CollegeName" v-model="academicData.collegeName" id="collegeName" required/>
+            <i v-show="errors.has('CollegeName')" class="fa fa-warning"></i>
+            <span v-show="errors.has('CollegeName')" class="help is-danger">{{ errors.first('CollegeName') }}</span>
           </div>
         </div>
         <div class="form-inline">
         <div class="col-md-6">
           <h6 class="labelStyle">Degree:</h6>
-            <input type="text" class="inputboxStyle" v-model="academicData.degree" id="degree" required/>
+            <input type="text" class="inputboxStyle" v-validate="'required|alpha'" :class="{'input': true, 'is-danger': errors.has('Degree') }" name="Degree" v-model="academicData.degree" id="degree" required/>
+          <i v-show="errors.has('Degree')" class="fa fa-warning"></i>
+            <span v-show="errors.has('Degree')" class="help is-danger">{{ errors.first('Degree') }}</span>
           </div>
         </div>
         <div class="form-inline">
         <div class="col-md-6"> 
             <h6 class="labelStyle">Branch:</h6>
-            <input type="text" class="inputboxStyle" v-model="academicData.branch" id="branch" required/>
+            <input type="text" class="inputboxStyle" v-validate="'required|alpha'" :class="{'input': true, 'is-danger': errors.has('Branch') }" name="Branch" v-model="academicData.branch" id="branch" required/>
+          <i v-show="errors.has('Branch')" class="fa fa-warning"></i>
+            <span v-show="errors.has('Branch')" class="help is-danger">{{ errors.first('Branch') }}</span>
           </div>
         </div>
         <div class="form-inline">
         <div class="col-md-6">
              <h6 class="labelStyle">Year Of Study:</h6>         
-            <input type="text" class="inputboxStyle" v-model="academicData.yearofstudy" id="yearofstudy" required/>
+            <input type="text" class="inputboxStyle" v-validate="'required'" :class="{'input': true, 'is-danger': errors.has('YearOfStudy') }" name="YearOfStudy" v-model="academicData.yearofstudy" id="yearofstudy" required/>
+          <i v-show="errors.has('YearOfStudy')" class="fa fa-warning"></i>
+            <span v-show="errors.has('YearOfStudy')" class="help is-danger">{{ errors.first('YearOfStudy') }}</span>
           </div>
         </div>
       </tab-content>
@@ -126,11 +140,14 @@
       onComplete: function () {
         this.updateStudent()
       },
-      personalCheck: function () {
-        // if (this.personalData.fname === '') {
-        //   return false
-        // }
-        return true
+      personalCheck: function (scope) {
+        this.$validator.validateAll(scope)
+        if (this.errors.any()) {
+          console.log('there are errors in the personal details tab', this.errors)
+          return false
+        } else {
+          return true
+        }
       },
       updateStudent: function () {
         const self = this
@@ -280,5 +297,8 @@
   -ms-border-radius:100%;
   -moz-border-radius:100%;
   -webkit-border-radius:100%;
+}
+.errmsg{
+  color: red;
 }
 </style>
