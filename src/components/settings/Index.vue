9334ed67e377aea0df187fe2fd85fc248e60ec41
Manimala <manimala1144@gmail.com>
2018-06-15 18:29:10 +0530

    UI - Change password is added to settings for students

--- src/components/settings/Index.vue
+++ src/components/settings/Index.vue
@@ -1,5 +1,33 @@
 <template>
-  <h3>coming soon...</h3>
+<div class="col-md-8">
+  <vuestic-tabs class="tabs" :names="['Password', 'SecondTab', 'ThirdTab']">
+  <div slot="Password" class="d-flex justify-content-center">
+		<div class="formStyle">
+			<div class="divStyle">
+			<label class="labelStyle" for="opswrd">Current Password:</label>
+			<input class="inputboxStyle" type="password" id="opswrd">
+		</div>
+		<div class="divStyle">
+			<label class="labelStyle" for="npswrd">New Password:</label>
+			<input class="inputboxStyle" type="password" id="npswrd">
+		</div>
+		<div class="divStyle">
+			<label class="labelStyle" for="cpswrd">Confirm Password:</label>
+			<input class="inputboxStyle" type="password" id="cpswrd">
+		</div>
+		<div class="divStyle">
+			<button class="button"><b>Next</b></button>
+		</div>
+  </div>
+	</div>
+	<div slot="SecondTab" class="d-flex justify-content-center">
+		
+  </div>
+	<div slot="ThirdTab" class="d-flex justify-content-center">
+   
+  </div>
+</vuestic-tabs>
+</div>
 </template>
 
 <script>
@@ -7,3 +35,55 @@
 	  name: 'settings'
 	}
 </script>
+
+<style scoped>
+.inputboxStyle{
+	  top: 4%;
+		float: right;
+    font-size: 15px;
+    width: 16rem;
+    padding: 6px 25px;
+    border: 1px solid #bdc7d8;
+    border-radius: 5px;
+    color:#333;
+  }
+	.divStyle{
+		padding-top: 4%;
+	}
+	.labelStyle{
+		margin-top: 1.6% !important;
+		color: #4ae387;
+		top: 4%;
+  }
+	.button{
+  background-color: #4ae387;
+	border: none;
+	margin-top: 5%;
+	margin-left: 1%;
+  width: 20%;
+  padding-top: 1%;
+  padding-bottom: 1%;
+	margin-left: 50%;
+}
+.formStyle{
+	margin-left: 0% !important;
+	top: 2.5% !important;
+}
+@media screen and (max-width: 650px){
+   .formStyle{
+     margin-left: 20px !important;
+     width: 100% !important;
+     top: 2.5% !important;
+    }
+		.inputboxStyle{
+	  margin-top: 2%;
+		width: 13rem !important;
+		float: left;
+		margin-left: 1% !important;
+		border: 1px solid #bdc7d8;
+  }
+	.vuestic-tabs .tab-content{
+		padding-top: 0.125rem !important;
+	}
+}
+</style>
