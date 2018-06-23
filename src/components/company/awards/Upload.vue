53a3884dc578299f83f281c9c2be4cab3052fb7b
Lenin <lenin-jaganathan>
2018-06-23 18:09:37 +0530

    Image upload and deletion in s3

--- src/components/company/awards/Upload.vue
+++ src/components/company/awards/Upload.vue
@@ -21,7 +21,11 @@ export default {
         url: 'http://localhost:3000/attachments',
         thumbnailWidth: 150,
         maxFilesize: 0.5,
-        headers: { Authorization: 'Bearer ' + this.token },
+        headers:
+        {
+          Authorization: 'Bearer ' + this.token,
+          path: 'company/awards'
+        },
         addRemoveLinks: true,
         dictDefaultMessage: '<i class="fa fa-cloud-upload"></i>UPLOAD ME'
       }
@@ -33,8 +37,9 @@ export default {
       this.$store.state.awardUploadedFile = []
       this.$store.state.awardUploadedFile.push({
         filename: file.name,
-        filepath: (response.url).slice(0, (response.url).indexOf('?')),
-        uploadpath: response.url
+        filepath: response.url.slice(0, response.url.indexOf('?')),
+        key: response.key,
+        path: '/company/awards'
       })
       console.log(
         'this store filepath',
@@ -45,34 +50,38 @@ export default {
       })
       axios({
         url: response.url,
-        method: 'put',
+        method: 'PUT',
+        ContentType: 'image/png',
         data: file
-      })
-      .then(function (res) {
-        console.log(res)
-      }, (err) => {
-        console.log(err)
-      })
+      }).then(
+        function (res) {
+          console.log(res)
+        },
+        err => {
+          console.log(err)
+        }
+      )
     },
     verror (err) {
       console.log(err)
     },
     vremoved (file, error, xhr) {
       const filename = file.name
-      const filepath = this.$store.state.awardUploadedFile.map(item => {
+      const item = this.$store.state.awardUploadedFile.map(item => {
         if (item.filename === filename) {
-          return item.filepath
+          return item
         }
       })
-      console.log('removed filepath', filepath)
-      this.$http({
+      console.log('removed filepath', item[0])
+      axios({
         method: 'delete',
         url: '/attachments',
         data: {
-          filepath: filepath
+          key: item[0].key
         },
         headers: {
-          Authorization: 'Bearer ' + this.token
+          Authorization: 'Bearer ' + this.token,
+          path: item[0].path
         }
       })
         .then(function (attachmentDeleted) {
