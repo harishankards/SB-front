9b9f84fb7610e4ad946b0f97995d1df08da43ebc
hs <hs@spritle.com>
2018-06-23 06:24:13 +0530

    DEBUG - Reactive data for charts in student stats #WIP

--- src/components/stats/Chart.vue
+++ src/components/stats/Chart.vue
@@ -1,13 +1,13 @@
 <script>
 import { Doughnut } from 'vue-chartjs'
+const { reactiveProp } = mixins
 
 export default {
   extends: Doughnut,
+  mixins: [reactiveProp],
   props: ['data', 'options'],
   mounted () {
     this.renderChart(this.data, this.options)
   }
 }
 </script>
-
-
