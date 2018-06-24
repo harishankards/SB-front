af704702bcdf694517b5fc85eb6483178616bfa7
hs <hs@spritle.com>
2018-06-24 22:22:27 +0530

    DEBUG - Student project analytics : reactive data props using watch function #failure #WIP

--- src/components/stats/Chart.vue
+++ src/components/stats/Chart.vue
@@ -5,6 +5,11 @@ export default {
   extends: Doughnut,
   mixins: [mixins.reactiveProp],
   props: ['chartData', 'options'],
+  watch: {
+    chartData () {
+      this.renderChart(this.chartData, this.options)
+    }
+  },
   mounted () {
     this.renderChart(this.chartData, this.options)
   }
